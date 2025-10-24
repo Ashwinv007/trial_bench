01f3a0acfcf474d21d18512940e5464c11ee6659
Ashwin V <klprogrammer39@gmail.com>
2025-10-24 11:48:36 +0530

    Fix Member inconsistency

--- src/store/Context.js
+++ src/store/Context.js
@@ -1,18 +1,17 @@
-import { createContext,useState } from "react";
+import { createContext, useState } from "react";
+import { auth, db } from '../firebase/config';
 
+export const FirebaseContext = createContext(null);
+export const AuthContext = createContext(null);
 
-export const FirebaseContext = createContext(null)
+export default function Context({ children }) {
+  const [user, setUser] = useState(null);
 
-export const AuthContext =createContext(null);
-
-
-export default function Context({children}){
-    const [user,setUser]= useState(null)
-
-    return(
-        <AuthContext.Provider value={{user,setUser}}>
-            {children} 
-        </AuthContext.Provider>
-    )
-
+  return (
+    <FirebaseContext.Provider value={{ auth, db }}>
+      <AuthContext.Provider value={{ user, setUser }}>
+        {children}
+      </AuthContext.Provider>
+    </FirebaseContext.Provider>
+  );
 }
